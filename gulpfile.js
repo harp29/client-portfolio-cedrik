dc1107cacfdc337f49b6839b5fa9f834bb761826
harp29 <brar.s.harpreet@gmail.com>
2017-05-15 14:31:02 -0400

    Broswer-Snyc added

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,40 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var relod = browserSync.reload;
 
+var SOURCEPATHS = {
+
+  sassSource : 'src/scss/*.scss'
+}
+
+var APPPATH = {
+  root: 'app/',
+  css : 'app/css',
+  js : 'app/js'
+}
 
 //sass/scss task
 gulp.task('sass', function(){
-  return gulp.src('src/scss/app.scss')
+  return gulp.src(SOURCEPATHS.sassSource)
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest(APPPATH.css))
 });
 
-//default task - make it run our sass
-gulp.task('default', ['sass']);
+//faster html processing on the fly
+gulp.task('serve', ['sass'], function(){
+  browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+    server : {
+      baseDir : APPPATH.root
+    }
+  })
+});
+
+//faster sass processing on the fly with - watch
+gulp.task('watch', ['serve', 'sass'], function(){
+  gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+})
+
+
+//default task - make it run our sass and serve - call watch
+gulp.task('default', ['watch']);
