483cbf497433362ce4bc0a2c955a39c425e48cc6
harp29 <brar.s.harpreet@gmail.com>
2017-05-17 01:58:35 -0400

    First Commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-var browserify = require('gulp-browserify');
 var autoprefixer = require('gulp-autoprefixer');
-var clean = require('gulp-clean');
+var browserify = require('gulp-browserify');
+var clean =require('gulp-clean');
 var concat = require('gulp-concat');
 var merge = require('merge-stream');
 var newer = require('gulp-newer');
@@ -12,140 +12,122 @@ var imagemin = require('gulp-imagemin');
 var injectPartials = require('gulp-inject-partials');
 var minify = require('gulp-minify');
 var rename = require('gulp-rename');
-var cssMin = require('gulp-cssmin');
+var cssmin = require('gulp-cssmin');
 var htmlmin = require('gulp-htmlmin');
 
-//src folder/directory
 var SOURCEPATHS = {
-
   sassSource : 'src/scss/*.scss',
+  sassApp: 'src/scss/app.scss',
   htmlSource : 'src/*.html',
   htmlPartialSource : 'src/partial/*.html',
-  jsSource   : 'src/js/**',
-  imageSource  : 'src/img/**'
-
+  jsSource : 'src/js/**',
+  imgSource : 'src/img/**'
 }
-
-//app folder/directory
-var APPPATH = {
-
+var APPPATH ={
   root: 'app/',
   css : 'app/css',
   js : 'app/js',
-  fonts : 'app/fonts',
-  img : 'app/img'
-
+  fonts: 'app/fonts',
+  img: 'app/img'
 }
 
-//clean - delete file if not in src directory
-gulp.task('clean-html', function(){
-  return gulp.src(APPPATH.root + '/*.html',
-    {read : false, force : true})
-  .pipe(clean());
+gulp.task('clean-html', function() {
+  return gulp.src(APPPATH.root + '/*.html', {read: false, force: true })
+      .pipe(clean());
+});
+gulp.task('clean-scripts', function() {
+  return gulp.src(APPPATH.js + '/*.js', {read: false, force: true })
+      .pipe(clean());
 });
 
-//clean - delete file if not in src directory
-gulp.task('clean-scripts', function(){
-  return gulp.src(APPPATH.js + '/*.js',
-    {read : false, force : true})
-  .pipe(clean());
+gulp.task('clean-images', function() {
+  return gulp.src(APPPATH.img + '/**', {read: false, force: true })
+      .pipe(clean());
 });
 
-//copy new image into directory/folder, auto minify images
-gulp.task('images', function(){
-  return gulp.src(SOURCEPATHS.imageSource)
-    .pipe(newer(APPPATH.img))
-    .pipe(imagemin())
-    .pipe(gulp.dest(APPPATH.img));
-})
+gulp.task('sass', function(){
 
-//move fonts - glyphicons
-gulp.task('moveFonts', function(){
-  gulp.src('./node_modules/bootstrap/dist/fonts/*.{eot,svg,ttf,woff,woff2}')
-      .pipe(gulp.dest(APPPATH.fonts))
-})
+  sassFiles = gulp.src(SOURCEPATHS.sassApp)
+      .pipe(autoprefixer())
+      .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+          .pipe(concat('app.css'))
+          .pipe(gulp.dest(APPPATH.css));
+});
 
-//copy scripts
-gulp.task('scripts', ['clean-scripts'], function(){
+gulp.task('images', ['clean-images'], function() {
+    return gulp.src(SOURCEPATHS.imgSource)
+      .pipe(newer(APPPATH.img))
+      .pipe(imagemin())
+      .pipe(gulp.dest(APPPATH.img));
+});
+
+
+gulp.task('scripts',['clean-scripts'],  function() {
   gulp.src(SOURCEPATHS.jsSource)
-  .pipe(concat('main.js'))
-  .pipe(browserify())
-  .pipe(gulp.dest(APPPATH.js))
-})
+      .pipe(concat('main.js'))
+      .pipe(browserify())
+      .pipe(gulp.dest(APPPATH.js))
+});
 
 /** Production Tasks **/
-//minify scripts
-gulp.task('compress', function(){
+gulp.task('compress',  function() {
   gulp.src(SOURCEPATHS.jsSource)
-  .pipe(concat('main.js'))
-  .pipe(browserify())
-    .pipe(minify())
-  .pipe(gulp.dest(APPPATH.js))
+      .pipe(concat('main.js'))
+      .pipe(browserify())
+      .pipe(minify())
+      .pipe(gulp.dest(APPPATH.js))
 });
 
-//sass/scss task
 gulp.task('compresscss', function(){
-  var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+
   var sassFiles;
+  sassFiles = gulp.src(SOURCEPATHS.sassSource)
+      .pipe(autoprefixer())
+      .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
 
-
-  sassFiles =  gulp.src(SOURCEPATHS.sassSource)
-    .pipe(autoprefixer())
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    return merge(sassFiles, bootstrapCSS)
-      .pipe(concat('app.css'))
-      .pipe(cssMin())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest(APPPATH.css));
+          .pipe(concat('app.css'))
+          .pipe(cssmin())
+          .pipe(rename({suffix: '.min'}))
+          .pipe(gulp.dest(APPPATH.css));
 });
 
-//minify Html
-gulp.task('minifyhtml', function(){
-  return gulp.src(SOURCEPATHS.htmlSource)
-  .pipe(injectPartials())
-  .pipe(htmlmin({collapseWhitespace: true}))
-  .pipe(gulp.dest(APPPATH.root))
-})
+gulp.task('minifyHtml', function() {
+   return gulp.src(SOURCEPATHS.htmlSource)
+        .pipe(injectPartials())
+        .pipe(htmlmin({collapseWhitespace:true}))
+        .pipe(gulp.dest(APPPATH.root))
+});
 
 /** End of Production Tasks **/
 
+gulp.task('html', function() {
+   return gulp.src(SOURCEPATHS.htmlSource)
+        .pipe(injectPartials())
+        .pipe(gulp.dest(APPPATH.root))
+});
+/*
+gulp.task('copy', ['clean-html'], function() {
+  gulp.src(SOURCEPATHS.htmlSource)
+      .pipe(gulp.dest(APPPATH.root))
+});
+*/
 
-//injectPartials Html
-gulp.task('html', function(){
-  return gulp.src(SOURCEPATHS.htmlSource)
-  .pipe(injectPartials())
-  .pipe(gulp.dest(APPPATH.root))
-})
-
-//copy files and paste them into another directory(app/)
-// gulp.task('copy', ['clean-html'], function(){
-//   gulp.src(SOURCEPATHS.htmlSource)
-//       .pipe(gulp.dest(APPPATH.root))
-// });
-
-
-//faster html processing on the fly
-gulp.task('serve', ['sass'], function(){
+gulp.task('serve', ['sass'], function() {
   browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
-    server : {
+    server: {
       baseDir : APPPATH.root
     }
   })
 });
 
+gulp.task('watch', ['serve', 'sass', 'clean-html', 'clean-scripts', 'scripts', 'images', 'html'], function() {
+    gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+    //gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
+    gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
+    gulp.watch([SOURCEPATHS.imgSource], ['images', 'clean-images']);
+    gulp.watch([SOURCEPATHS.htmlSource, SOURCEPATHS.htmlPartialSource], ['html','clean-html']);
+} );
 
-//faster sass processing on the fly with - watch
-gulp.task('watch', ['serve', 'sass', 'clean-html', 'clean-scripts',
-'scripts', 'moveFonts', 'images', 'html'], function(){
-  gulp.watch([SOURCEPATHS.sassSource], ['sass']);
-  // gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
-  gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
-  gulp.watch([SOURCEPATHS.htmlSource, SOURCEPATHS.htmlPartialSource], ['html']);
-})
-
-
-//default task - make it run our sass and serve - call watch
 gulp.task('default', ['watch']);
 
-//task for production
-gulp.task('production', ['minifyhtml', 'compresscss', 'compress']);
+gulp.task('production', ['minifyHtml', 'compresscss', 'compress'] );
